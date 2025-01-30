252906ba4dec3aeeb2d139dd2dfb15662bb0aa74
kenseiiy <kentdominicvicentenavaluna@gmail.com>
2025-01-30 21:45:42 +0800

    Initial Commit

---
+++ NAVALUNA_LabAct1/Pages/Shared/news3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NAVALUNA_LabAct1.Pages.Shared
+{
+    public class news3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
